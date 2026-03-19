f729d2e91d1ffaaf817ed33faec2d1c342b6274b
Kesia Pimenta <kesiapimentaferreira525@gmail.com>
2026-03-19 08:17:12 -0300

    arquivos iniciais do projeto

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# techboard-react
+
+techboard-react
---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+
+
+// no react , componentes sao FUNÇOES
+
+function FormularioDeEventos(){
+
+  return(
+        <form className="form-evento">
+      <h2>Preencha para criar um evento:</h2>    
+      <fieldset>
+        <label htmlFor="nome">
+          Qual o nome do evento?
+        </label>
+        <input type="text" id='nome'/>
+      </fieldset>
+        </form>
+
+
+  )
+
+}
+function App() {
+
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+      </header>  
+
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>  
+
+      <FormularioDeEventos/>     
+    </main>
+  );
+}
+
+export default App;
